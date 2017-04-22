c4af37fa8e81b394db62f7af432a080c383fe9fe
webbdm <gwebb762@gmail.com>
2017-04-21 22:28:42 -0500

    Restructed promise and added functions for writing to DOM

--- javascript/main.js
+++ javascript/main.js
@@ -4,62 +4,64 @@ $(document).ready(function() {
     let user1 = [];
     let user2 = [];
 
-    $("button").click(function() {
-        loadUser($("#userName1").val());
-        loadUser($("#userName2").val());
-    });
-
-
     const loadUser = (userInput) => {
         return new Promise((resolve, reject) => {
             let userString = `https://teamtreehouse.com/${userInput}.json`;
-            console.log(userString);
-            $.ajax("https://teamtreehouse.com/geoffwebb.json")
+            $.ajax(userString)
                 .done((data1) => resolve(data1))
                 .fail((error) => reject(error));
         });
-
-        //cageMatch();
     };
 
+    const printUsers = (user1, user2) => {
+        let newString = "";
+        newString += `<div class="row" id="contestants">`;
+        newString += buildUser(user1);
+        newString += buildUser(user2);
+        newString += `</div>`;
+        return newString;
+    };
 
-
-    const writeToDOM = (user1, user2) => {
-
-        console.log(user1, user2);
+    const buildUser = (user) => {
         let domString = "";
-
-        // .map here
-
-        outputContainer.append(domString);
+        domString += ` <div class="col-md-6 thumbnail">
+                      <h3>${user.name}</h3>
+                      <img src=${user.gravatar_url} class="gravatar" alt="image">
+                      <h2>${user.points.total} Points</h2>
+                      <h3 id="win">WINNER</h3>
+                  </div>`;
+        return domString;
     };
 
+    const invalidUsername = () => {
+        $("#errorMessage").show();
+    };
 
-    const cageMatch = () => {
-        loadUser().then((user) => {
-                // user1.forEach(function(user) {
-                //     user1.push(user);
-                // });
+    $("button").click(function() {
+        outputContainer.empty();
+        $("#errorMessage").hide();
+        let userInput1 = $("#userName1").val();
+        let userInput2 = $("#userName2").val();
+        cageMatch(userInput1, userInput2);
+    });
 
-                Promise.all([loadUser()])
-                    .then((result) => {
-                        console.log(result);
-                        // result.forEach((xhrResult) => {
-                        //     xhrResult.forEach((animal) => {
-                        //         myAnimals.push(animal);
-                        //     });
-                        // });
+    const cageMatch = (user1, user2) => {
+        Promise.all([loadUser(user1), loadUser(user2)])
+            .then((result) => {
+                let loadedUser1 = result[0];
+                let loadedUser2 = result[1];
+                outputContainer.append(printUsers(loadedUser1, loadedUser2));
 
-                        // .map the user
-                        //writeToDOM(user1,user2);
-                    })
-                    .catch(function(error) {
-                        console.log(error);
-                    });
+                if (loadedUser1.points.total > loadedUser2.points.total) {
+                    console.log($("#contestants").children().first());
+
+                } else {
+                    console.log($("#contestants").children().second());
+                };
+
             })
-            .catch(function(Error) {
-                console.log(Error);
+            .catch(function(error) {
+                invalidUsername()
             });
-    }
-
+    };
 });
